821b4b2e761c4ccf1ef83459081b05a195a6fae7
Thanasis Georgiou <contact@thgeorgiou.com>
2016-03-21 11:58:10 +0200

    Initial commit

---
+++ LED-Fireplace.ino
@@ -0,0 +1,123 @@
+#include <SmartMatrix3.h>
+#include <FastLED.h> // For some macros, maybe this dependency can be dropped
+
+// Matrix
+#define COLOR_DEPTH 24
+const uint8_t kMatrixWidth = 32;		// 32x16
+const uint8_t kMatrixHeight = 16;
+const uint8_t kRefreshDepth = 36;		// Known to work with this display, 32x32 would require another value
+const uint8_t kDmaBufferRows = 4;       // We don't have any memory constraints
+const uint8_t kPanelType = SMARTMATRIX_HUB75_16ROW_MOD8SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
+const uint8_t kMatrixOptions = (SMARTMATRIX_OPTIONS_NONE);      // see http://docs.pixelmatix.com/SmartMatrix for options
+const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
+
+/* Allocate buffer and one layer */
+SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
+SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
+
+/* How quickly does the flame cool? */
+byte cooling = 100;
+
+/* Change of making */
+byte sparking = 30;
+
+/* Buffer for flames */
+byte _heat[32 * 16];
+
+/* Whether the screen is currently running? For sleepmode */
+bool isRunning = true;
+
+/* Return a color from the palette */
+rgb24 getColor(int index);
+
+/* First time setup */
+void setup() {
+  // Prepare matrix
+  matrix.addLayer(&backgroundLayer);
+  matrix.begin();
+  matrix.setBrightness(100 * (255 / 100)); // Max brightness
+
+  // Enable color correction
+  backgroundLayer.enableColorCorrection(true);
+
+  // Empty screen
+  backgroundLayer.fillScreen({0, 0, 0});
+  backgroundLayer.swapBuffers();
+}
+
+void loop() {
+  // Get a capacitive touch reading on pin 15
+  uint16_t touch = touchRead(15);
+  
+  // If the user is touching the bolt
+  if (touch > 2000) {
+    // Turn the display on/off
+    isRunning = !isRunning;
+
+    if (isRunning == false) {
+      backgroundLayer.fillScreen({0, 0, 0});
+      backgroundLayer.swapBuffers();
+    }
+  }
+
+  // Some primitive debouncing
+  while(touchRead(15) > 2000) {}
+
+  // If the display is not running, no need to render the frame
+  if (isRunning == false) return;
+
+  // Step 1. Cool down every cell a little
+  for (int x = 0; x < 32; x++) {
+    for (int y = 0; y < 16; y++) {
+      _heat[(y * 32) + x] = qsub8(_heat[(y * 32) + x], random(0, ((cooling * 10) / 16) + 2));
+    }
+
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for (int y = 0; y < 16; y++) {
+      _heat[(y * 32) + x] = (byte)(
+                              (_heat[(((y + 1 > 13) ? 13 : y + 1) * 32) + x] +
+                               _heat[(((y + 2 > 13) ? 13 : y + 2) * 32) + x] +
+                               _heat[(((y + 3 > 13) ? 13 : y + 3) * 32) + x]) / 3
+                            );
+    }
+
+    // Step 3.  Randomly ignite new 'sparks' of heat
+    if (random(0, 233) < sparking) {
+      // int x = (p[0] + p[1] + p[2]) / 3;
+
+      _heat[(13 * 32) + x] = qadd8(_heat[(13 * 32) + x], random(160, 233));
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for (int y = 0; y < 16; y++) {
+      byte colorIndex = _heat[(y * 32) + x];
+
+      // Recommend that you use values 0-240 rather than
+      // the usual 0-233, as the last 13 colors will be
+      // 'wrapping around' from the hot end to the cold end,
+      // which looks wrong.
+      colorIndex = scale8(colorIndex, 240);
+
+      // Draw pixel
+      rgb24 color = getColor(colorIndex);
+      backgroundLayer.drawPixel(x, y, color);
+    }
+  }
+
+  // Refresh screen
+  backgroundLayer.swapBuffers();
+  delay(13);
+}
+
+/* Returns a color from the palette */
+/* Red -> Orange -> White */
+rgb24 getColor(int index) {
+  byte b = (byte) (index * (399 / 233));
+
+  return {
+    (uint8_t) b,
+    (uint8_t) scale8(qsub8(b, 83), 233),
+    (uint8_t) scale8(qsub8(8, 83 * 2), 233)
+  };
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # LED-Fireplace
 A little LED panel creating atmosphere
+
+Using a [Teensy 3.1](https://www.pjrc.com/teensy/) and [a 32x16 Pixel Matrix](https://www.adafruit.com/product/420)
+I created a small LED fireplace which provides some nice ambience to the room at dark. The code is heavily based on
+the [Aurora](https://github.com/pixelmatix/aurora), a more complete art display project.
+
+## Usage
+The code depands on the [SmartMatrix3]() and [FastLED](https://github.com/FastLED/FastLED) libraries. Both are available
+through the Arduino Library manager.
+
+## Features/TODO
+Currently the fire is rendered and the display can be turned on/off by touching a bolt on the frame. In the future I want
+to add a clock and make it configurable (set time, brightness, auto turn on/off) through serial to minize the amount of
+input channels needed.
+
+# License
+My small code snippet is available in the Public Domain. All libraries are copyrighted by their respective authors.
+For more info check the LICENSE file.
